bfb6443e21847dfec1b9c4976b26686b1ba0f439
7986769088 <st7879singh@gmail.com>
2026-05-30 22:34:55 +0530

    the mail commit

--- shieldher-backend/services/otpService.js
+++ shieldher-backend/services/otpService.js
@@ -7,22 +7,14 @@ const logger = require("../utils/logger");
 console.log("OTP SERVICE LOADED");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  host: "smtp-relay.brevo.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-  tls: {
-    rejectUnauthorized: false
   }
 });
-
 transporter.verify((err, success) => {
   if (err) {
     console.log("SMTP ERROR:", err);
