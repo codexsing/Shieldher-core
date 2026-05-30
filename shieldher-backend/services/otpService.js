9a09440bda1ddb095b12a34eb525040ed5ccabe0
7986769088 <st7879singh@gmail.com>
2026-05-30 22:00:18 +0530

    the new otp service

--- shieldher-backend/services/otpService.js
+++ shieldher-backend/services/otpService.js
@@ -4,11 +4,12 @@ const { Otp } = require("../models/SafeZoneOtp");
 const { otpEmail } = require("../utils/emailTemplates");
 const logger = require("../utils/logger");
 
+console.log("OTP SERVICE LOADED");
+
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
   secure: false,
-
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
@@ -27,7 +28,11 @@ const generateOtp = () =>
   crypto.randomInt(100000, 999999).toString();
 
 const sendOtp = async (email, name) => {
+  console.log("SEND OTP CALLED:", email);
+
   const otp = generateOtp();
+
+  console.log("OTP GENERATED:", otp);
 
   const expiresAt = new Date(
     Date.now() + 10 * 60 * 1000
@@ -41,26 +46,34 @@ const sendOtp = async (email, name) => {
     expiresAt
   });
 
+  console.log("OTP SAVED IN DB");
+
   const { subject, html } = otpEmail({
     name,
     otp
   });
 
   const mailOptions = {
-    from: process.env.EMAIL_USER,
+    from: process.env.EMAIL_FROM || process.env.EMAIL_USER,
     to: email,
     subject,
     html
   };
 
   try {
+    console.log("BEFORE SENDMAIL");
+
     const info = await transporter.sendMail(mailOptions);
 
+    console.log("AFTER SENDMAIL");
     console.log("MAIL SENT:", info.response);
 
     logger.info(`OTP sent to ${email}`);
+
+    return true;
   } catch (err) {
     console.log("MAIL ERROR:", err);
+    throw err;
   }
 };
 
@@ -92,7 +105,6 @@ const verifyOtp = async (email, otp) => {
   }
 
   record.used = true;
-
   await record.save();
 
   return {
